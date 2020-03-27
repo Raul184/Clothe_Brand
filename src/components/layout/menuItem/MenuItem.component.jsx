e2897b8735ca88fafdc0c091457f61317cb31da8
Snash <raulmv01@hotmail.es>
2020-03-27 19:09:54 +0100

    styles on homepage set

--- src/components/layout/menuItem/MenuItem.component.jsx
+++ src/components/layout/menuItem/MenuItem.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const MenuItem = ({ title , subtitle , img , size }) => (
-  <div className={`${size} menu-item`} style={{backgroundImage: `url(${img})`}}>
+  <div className={`${size} menu-item`}>
+    <div className="background-img" style={{backgroundImage: `url(${img})`}}>
+    </div>
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">{subtitle}</span>
     </div>
   </div>
