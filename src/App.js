65d8e052b99210514771e8f084c2ac3371003e42
Snash <raulmv01@hotmail.es>
2020-03-28 20:47:40 +0100

    contact route and page attached

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/layout/header/Header.component'
 //Pages
 import Homepage from './components/pages/home/HomePage.component'
 import ShopPage from './components/pages/shop/ShopPage.component'
-
+import { ContactPage } from './components/pages/contact/ContactPage.component'
 //Routing
 import { Route , Switch } from 'react-router-dom'
 
@@ -17,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/contact' component={ContactPage} />
       </Switch>
     </div>
   );
