7dcdc4957dda19dc2fa2dcd676a95266995408b2
Snash <raulmv01@hotmail.es>
2020-04-15 12:38:47 +0100

    sagas implemented for ShopPage comp.

--- src/App.js
+++ src/App.js
@@ -72,11 +72,7 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  { setCurrentUser }
 )(App);
--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,13 +1,42 @@
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery , call , put } from 'redux-saga/effects'
 import {
   FETCH_COLLECTIONS_START ,
   FETCH_COLLECTIONS_SUCCESS ,
   FETCH_COLLECTIONS_FAILURE
 } from './shop.types'
 
+import { 
+  fetchCollectionsSuccess , 
+  fetchCollectionsFailure 
+} from './shop.actions'
+
+// FIREBASE
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
+
+
 export function* fetchCollectionsAsync() {
-  console.log('ran');
+  // collectionRef.get().then(snapshot=> {
+  //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+  //   dispatch(action(collectionsMap))
+  // })
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get()
+    const collectionsMap = yield call( convertCollectionsSnapshotToMap , snapshot );
+    yield put(
+      fetchCollectionsSuccess(collectionsMap)
+    );
+  } 
+  catch (error) {
+    yield put(
+      fetchCollectionsFailure(error.message)
+    )
+  }
 }
+
 
 export function* fetchCollectionsStart() {
   yield takeEvery( 
