8656f7dfc1d3d54be5891208471f55ff634bf932
Snash <raulmv01@hotmail.es>
2020-04-28 21:25:06 +0100

    performance updated on Shop displayer of categories

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,32 +1,30 @@
-import React , { useEffect } from 'react';
+import React , { useEffect , lazy , Suspense } from 'react';
 import { Route } from 'react-router-dom';
 // Redux
 import { connect } from 'react-redux';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
-// Comps
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component'
 // Loading Spinner
 import Spinner from '../../components/with-spinner/with-spinner.component';
 // Memoization
 import { createStructuredSelector } from 'reselect'
 import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors'
+// Comps.
+const CollectionsOverview = lazy(() => import('../../components/collections-overview/collections-overview.component'))
+const CollectionPage = lazy(() => import('../collection/collection.component'))
 
 
-export const ShopPage = ({ match , fetchCollectionsStart , isLoading }) => {
+export const ShopPage = ({ match , fetchCollectionsStart }) => {
   useEffect(() => {
     fetchCollectionsStart();
-  }, //eslint-disable-next-line 
-  [])
+  },  
+  [fetchCollectionsStart])
+
   return (
     <div className='shop-page'>
-      {
-        isLoading ? 
-        <Spinner /> 
-        : 
+      <Suspense fallback={<Spinner />}>
         <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-      }
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>      
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>      
+      </Suspense>
     </div>
   );
 }
