c195ffaefaebe506fa1c8a80d4a96ef51e75c8e3
Snash <raulmv01@hotmail.es>
2020-05-04 21:19:29 +0100

    compression library for gzipping on clientSide

--- server.js
+++ server.js
@@ -2,37 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const compression = require('compression');
 
 if(process.env.NODE_ENV === 'development') require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use( compression() );
 // Req => access as a json file
-app.use(bodyParser.json());
+app.use( bodyParser.json() );
 // urls strings properly formatted
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use( bodyParser.urlencoded({ extended: true }) )
 // App web server hosted in a != origin from users
-app.use(cors());
+app.use( cors() );
 
 // On Hk Server
-if(process.env.NODE_ENV === 'production'){
+if( process.env.NODE_ENV === 'production'){
   // serving clientApp
-  app.use(express.static(path.join(__dirname , 'client/build')));
+  app.use( express.static(path.join(__dirname , 'client/build')));
 }
 // For ALL reqs ==> build.js
-app.get('*' , (req , res ) => res.sendFile(
-  path.join(__dirname , 'client/build' , 'index.html')
+app.get( '*' , (req , res ) => res.sendFile(
+  path.join( __dirname , 'client/build' , 'index.html')
   )
 )
 
 // Router
 // Stripe Payments
-app.use('/payment' , require('./routes/stripe.js'))
+app.use( '/payment' , require('./routes/stripe.js'))
 
 
-app.listen(port , error => {
+app.listen( port , error => {
   if(error) throw error; 
   console.log('Server running on port: ' + port);  
 });
