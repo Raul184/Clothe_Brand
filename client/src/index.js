f7edac91c8a3aaa8ddb87aa2c58d2500396f11c7
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-02 16:20:19 +0100

    refactoring login/signup page

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,6 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import * as ServiceWorker from './serviceWorker';
-
-// import './index.css';
 import App from './App';
 
 const MyAppWithStore = () => (
